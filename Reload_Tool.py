a4ec304db7bb4dea7a82a06a422950b4f2ce65cb
Casalis <georgecdep@gmail.com>
2017-03-17 12:20:47 +0000

    Uploaded Reload Tool

---
+++ Reload_Tool.py
@@ -0,0 +1,36 @@
+from robotide.publish import RideOpenSuite, RideFileNameChanged
+from robotide.publish.messages import RideNewProject, RideSaved
+from robotide.pluginapi import Plugin# ActionInfo, SeparatorInfo
+
+
+import wx.lib.inspection
+wx.lib.inspection.InspectionTool().Show()
+
+class Reload_Files(Plugin):
+    """Used to reload files edited in external editor via shortcut."""
+
+
+    def __init__(self, application):
+        Plugin.__init__(self, application, metadata={'Shortcut': 'CTRL + R'})
+        self.path = "None"
+        self.reg_shortcut()
+
+    def reg_shortcut(self):
+        self.register_shortcut('CtrlCmd-D', self.reload_files)
+        self.subscribe(self.set_path, RideOpenSuite)
+        self.subscribe(self.set_path, RideFileNameChanged)
+        self.subscribe(self.set_path, RideNewProject)
+        self.subscribe(self.set_path, RideSaved)
+
+    def enable(self):
+        print("Reload Enabled")
+
+    def reopen_suite(self):
+        self.open_suite(self.path)
+
+    def reload_files(self,event):
+        self.reopen_suite()
+
+    def set_path(self,event):
+        self.path = event.path
+
